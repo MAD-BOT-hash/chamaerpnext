ad7f12398873cd06089dab29b8ca3556dc84c7f6
MAD-BOT-hash <sammynduati@gmail.com>
2025-11-17 15:57:07 +0300

    pmtnew13

---
+++ shg/shg/doctype/shg_bulk_payment_item/shg_bulk_payment_item.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2025, SHG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SHG Bulk Payment Item', {
+	// refresh: function(frm) {
+	// }
+});
