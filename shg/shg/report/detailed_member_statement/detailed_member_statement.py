2ea3a64647083c52ae28b444015b4825c1e0e270
MAD-BOT-hash <sammynduati@gmail.com>
2025-11-18 12:17:00 +0300

    reptn4

--- shg/shg/report/detailed_member_statement/detailed_member_statement.py
+++ shg/shg/report/detailed_member_statement/detailed_member_statement.py
@@ -1,6 +1,6 @@
 import frappe
 from frappe import _
-from frappe.utils import getdate, flt
+from frappe.utils import flt
 
 
 def execute(filters=None):
@@ -16,9 +16,9 @@ def execute(filters=None):
 def get_columns():
     return [
         {"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 120},
-        {"label": "DocType", "fieldname": "document_doctype", "fieldtype": "Data", "width": 150},
-        {"label": "Document", "fieldname": "document_link", "fieldtype": "Link", "options": "document_doctype", "width": 150},
-        {"label": "Description", "fieldname": "description", "fieldtype": "Data", "width": 300},
+        {"label": "Document Type", "fieldname": "document_doctype", "fieldtype": "Data", "width": 180},
+        {"label": "Document", "fieldname": "document_link", "fieldtype": "Link", "options": "document_doctype", "width": 160},
+        {"label": "Description", "fieldname": "description", "fieldtype": "Data", "width": 350},
         {"label": "Debit (KES)", "fieldname": "debit", "fieldtype": "Currency", "width": 120},
         {"label": "Credit (KES)", "fieldname": "credit", "fieldtype": "Currency", "width": 120},
         {"label": "Running Balance (KES)", "fieldname": "balance", "fieldtype": "Currency", "width": 150},
@@ -33,32 +33,22 @@ def get_data(filters):
     if not member:
         return []
 
-    # Build date filter conditions
-    date_conditions = []
-    params = {"member": member}
-    
-    if from_date:
-        date_conditions.append("t.date >= %(from_date)s")
-        params["from_date"] = from_date
-        
-    if to_date:
-        date_conditions.append("t.date <= %(to_date)s")
-        params["to_date"] = to_date
-    
-    # Join all date conditions
     date_filter = ""
-    if date_conditions:
-        date_filter = " AND " + " AND ".join(date_conditions)
+    params = {"member": member}
+
+    if from_date and to_date:
+        date_filter = " AND date BETWEEN %(from_date)s AND %(to_date)s"
+        params.update({"from_date": from_date, "to_date": to_date})
+    elif from_date:
+        date_filter = " AND date >= %(from_date)s"
+        params.update({"from_date": from_date})
+    elif to_date:
+        date_filter = " AND date <= %(to_date)s"
+        params.update({"to_date": to_date})
 
     query = f"""
-        SELECT 
-            t.date,
-            t.document_doctype,
-            t.document_link,
-            t.description,
-            t.debit,
-            t.credit
-        FROM (
+        SELECT * FROM (
+            -- Contributions
             SELECT
                 c.contribution_date AS date,
                 'SHG Contribution' AS document_doctype,
@@ -68,10 +58,11 @@ def get_data(filters):
                 c.amount AS credit
             FROM `tabSHG Contribution` c
             LEFT JOIN `tabSHG Contribution Type` ct ON ct.name = c.contribution_type
-            WHERE c.member = %(member)s AND c.docstatus = 1
+            WHERE c.member = %(member)s AND c.docstatus = 1 {date_filter}
 
             UNION ALL
 
+            -- Contribution Invoices
             SELECT
                 i.invoice_date AS date,
                 'SHG Contribution Invoice' AS document_doctype,
@@ -80,10 +71,11 @@ def get_data(filters):
                 0 AS debit,
                 i.amount AS credit
             FROM `tabSHG Contribution Invoice` i
-            WHERE i.member = %(member)s AND i.docstatus = 1
+            WHERE i.member = %(member)s AND i.docstatus = 1 {date_filter}
 
             UNION ALL
 
+            -- Meeting Fines
             SELECT
                 f.fine_date AS date,
                 'SHG Meeting Fine' AS document_doctype,
@@ -92,10 +84,11 @@ def get_data(filters):
                 0 AS debit,
                 f.fine_amount AS credit
             FROM `tabSHG Meeting Fine` f
-            WHERE f.member = %(member)s AND f.docstatus = 1
+            WHERE f.member = %(member)s AND f.docstatus = 1 {date_filter}
 
             UNION ALL
 
+            -- Loan Disbursements
             SELECT
                 l.disbursement_date AS date,
                 'SHG Loan' AS document_doctype,
@@ -104,10 +97,11 @@ def get_data(filters):
                 l.loan_amount AS debit,
                 0 AS credit
             FROM `tabSHG Loan` l
-            WHERE l.member = %(member)s AND l.docstatus = 1
+            WHERE l.member = %(member)s AND l.docstatus = 1 {date_filter}
 
             UNION ALL
 
+            -- Loan Repayments
             SELECT
                 r.repayment_date AS date,
                 'SHG Loan Repayment' AS document_doctype,
@@ -116,10 +110,11 @@ def get_data(filters):
                 0 AS debit,
                 r.total_paid AS credit
             FROM `tabSHG Loan Repayment` r
-            WHERE r.member = %(member)s AND r.docstatus = 1
+            WHERE r.member = %(member)s AND r.docstatus = 1 {date_filter}
 
             UNION ALL
 
+            -- Payment Entries (money received)
             SELECT
                 pe.payment_date AS date,
                 'SHG Payment Entry' AS document_doctype,
@@ -128,15 +123,13 @@ def get_data(filters):
                 0 AS debit,
                 pe.total_amount AS credit
             FROM `tabSHG Payment Entry` pe
-            WHERE pe.member = %(member)s AND pe.docstatus = 1
-        ) AS t
-        WHERE 1=1 {date_filter}
-        ORDER BY t.date, t.document_doctype, t.document_link
+            WHERE pe.member = %(member)s AND pe.docstatus = 1 {date_filter}
+        ) AS combined
+        ORDER BY date ASC, document_doctype ASC, document_link ASC
     """
 
     rows = frappe.db.sql(query, params, as_dict=True)
 
-    # Calculate running balance
     balance = 0
     for r in rows:
         balance += flt(r.credit) - flt(r.debit)
