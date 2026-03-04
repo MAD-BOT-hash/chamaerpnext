ee376d79615360cef1e63263538a219e97794761
MAD-BOT-hash <sammynduati@gmail.com>
2026-03-04 20:37:32 +0300

    en61

--- shg/shg/services/payment/__init__.py
+++ shg/shg/services/payment/__init__.py
@@ -13,11 +13,33 @@ from .allocation_engine import (
     OverpaymentError,
     DocumentNotFoundError,
     DocumentLockedError,
+    AlreadyPaidError,
+    # Status constants
+    STATUS_UNPAID,
+    STATUS_PENDING,
+    STATUS_PARTIALLY_PAID,
+    STATUS_PAID,
+    STATUS_WAIVED,
+    STATUS_CANCELLED,
+    STATUS_OVERDUE,
+    PAYABLE_STATUSES,
+    SETTLED_STATUSES,
 )
 
 from .bulk_payment_service import (
     BulkPaymentService,
     BulkPaymentServiceError,
+    # Query functions
+    get_unpaid_invoices_for_company,
+    get_unpaid_invoices_for_member,
+    get_unpaid_contributions_for_company,
+    get_unpaid_contributions_for_member,
+    get_unpaid_meeting_fines_for_company,
+    get_unpaid_meeting_fines_for_member,
+    get_unpaid_loan_installments_for_company,
+    get_unpaid_loan_installments_for_member,
+    get_all_unpaid_items_for_company,
+    get_all_unpaid_items_for_member,
 )
 
 __all__ = [
@@ -33,8 +55,32 @@ __all__ = [
     'OverpaymentError',
     'DocumentNotFoundError',
     'DocumentLockedError',
+    'AlreadyPaidError',
+    
+    # Status constants
+    'STATUS_UNPAID',
+    'STATUS_PENDING',
+    'STATUS_PARTIALLY_PAID',
+    'STATUS_PAID',
+    'STATUS_WAIVED',
+    'STATUS_CANCELLED',
+    'STATUS_OVERDUE',
+    'PAYABLE_STATUSES',
+    'SETTLED_STATUSES',
     
     # Bulk Payment Service
     'BulkPaymentService',
     'BulkPaymentServiceError',
+    
+    # Query Functions
+    'get_unpaid_invoices_for_company',
+    'get_unpaid_invoices_for_member',
+    'get_unpaid_contributions_for_company',
+    'get_unpaid_contributions_for_member',
+    'get_unpaid_meeting_fines_for_company',
+    'get_unpaid_meeting_fines_for_member',
+    'get_unpaid_loan_installments_for_company',
+    'get_unpaid_loan_installments_for_member',
+    'get_all_unpaid_items_for_company',
+    'get_all_unpaid_items_for_member',
 ]
